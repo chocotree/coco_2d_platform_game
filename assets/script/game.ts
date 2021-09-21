14c63b19d19ae257724f70a3efae13e57797081e
IsTree <IsTree@gmail.com>
2021-09-21 14:43:28 +0800

    draw the wall physicsBox

--- assets/script/game.ts
+++ assets/script/game.ts
@@ -16,13 +16,51 @@ export default class NewClass extends cc.Component {
     @property
     text: string = 'hello';
 
-    // LIFE-CYCLE CALLBACKS:
+    onLoad() {
+        const physicsManager = cc.director.getPhysicsManager();
+        physicsManager.enabled = true;
+        // physicsManager.debugDrawFlags = 1;
 
-    // onLoad() {}
-
-    start() {
-
+        this.initMapNode(this.node);
     }
+
+    initMapNode(ccNode: cc.Node) {
+        // mapNode -> has wall
+        const tiledMap = ccNode.getComponentInChildren(cc.TiledMap);
+        const tiledSize = tiledMap.getTileSize();
+        const layer = tiledMap.getLayer('wall');
+        const layerWallSize = layer.getLayerSize();
+
+        for (let i = 0; i < layerWallSize.width; i++) {
+            for (let j = 0; j < layerWallSize.height; j++) {
+                const tiled = layer.getTiledTileAt(i, j, true);
+                this.drawWallPhysicsBoxColliders({
+                    tiled,
+                    colliderSize: tiledSize,
+                })
+            }
+        }
+    }
+
+    drawWallPhysicsBoxColliders(params: {
+        tiled: cc.TiledTile,
+        colliderSize: cc.Size,
+    }) {
+        const { tiled, colliderSize } = params;
+
+        if (tiled.gid !== 0) {
+            tiled.node.group = 'wall';
+
+            const body = tiled.node.addComponent(cc.RigidBody);
+            body.type = cc.RigidBodyType.Static;
+            const collider = tiled.node.addComponent(cc.PhysicsBoxCollider);
+            collider.offset = cc.v2(colliderSize.width / 2, colliderSize.height / 2);
+            collider.size = colliderSize;
+            collider.apply();
+        }
+    }
+
+    // start() {}
 
     // update (dt) {}
 }
